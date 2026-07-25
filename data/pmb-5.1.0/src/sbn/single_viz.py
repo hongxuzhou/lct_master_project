26bea76a58e8f768e3d4ef43e8d090667b8bc79d
Hongxu <68404495+hongxuzhou@users.noreply.github.com>
2026-07-25 19:58:49 +0200

    Update the viz and make sure the notation doc on Overleaf aligns with the latest viz

--- data/pmb-5.1.0/src/sbn/single_viz.py
+++ data/pmb-5.1.0/src/sbn/single_viz.py
@@ -5,7 +5,7 @@ This script is used to generate small-scale DAGs from SBN sequences
 from sbn_smatch import SBNGraph
 
 # Manually paste the SBN sequence here, or later upgrade to argparse
-sbn = "person.n.01 Name \"Joe\" time.n.01 TPR now buy.v.01 Agent -2 Time -1 Theme +1 Manner +3 CORRECTION <1  car.n.01 Name \"Ferrari\" CONJUNCTION <2 car.n.01 Name \"Jaguar\" ThemeOf -2  gift.n.01 PartOf \"birthday\" MannerOf -3  CORRECTION <1  male.n.01 EQU -6  buy.v.01 Agent -1 Theme -3 Manner +1 gift.n.01 PartOf \"wedding\" "
+sbn = "NEGATION <1 farmer.n.01 own.v.01 Pivot -1 Theme +1 donkey.n.01 NEGATION <1 CORRECTION <1 beat.v.01 Agent -3 Theme +2 CONJUNCTION <2 feed.v.01 Agent -4 Theme +1 entity.n.01 EQU -3"   
 
 try: 
     g = SBNGraph().from_string(sbn, is_single_line=True)
