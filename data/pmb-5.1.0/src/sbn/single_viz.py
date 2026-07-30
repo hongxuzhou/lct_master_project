ec555934917dd1259ae318c9d7ce679f94e983d6
Hongxu <68404495+hongxuzhou@users.noreply.github.com>
2026-07-30 17:55:13 +0200

    Manual done

--- data/pmb-5.1.0/src/sbn/single_viz.py
+++ data/pmb-5.1.0/src/sbn/single_viz.py
@@ -5,7 +5,7 @@ This script is used to generate small-scale DAGs from SBN sequences
 from sbn_smatch import SBNGraph
 
 # Manually paste the SBN sequence here, or later upgrade to argparse
-sbn = "NEGATION <1 farmer.n.01 own.v.01 Pivot -1 Theme +1 donkey.n.01 NEGATION <1 CORRECTION <1 beat.v.01 Agent -3 Theme +2 CONJUNCTION <2 feed.v.01 Agent -4 Theme +1 entity.n.01 EQU -3"   
+sbn = "female.n.02  CORRECTION <1  time.n.08 TSU now  go.v.01 Theme -2 Time -1                   CONJUNCTION <2                       time.n.08 TPR now                          go.v.01 Theme -4 Time -1 Destination +1    church.n.01                                "   
 
 try: 
     g = SBNGraph().from_string(sbn, is_single_line=True)
